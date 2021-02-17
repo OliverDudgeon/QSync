5280ce937b622f0fb9ae25bc139ce7a91564113d
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-02-17 12:42:50 +0000

    Add a qfunc plot method to utils

--- utils.py
+++ utils.py
@@ -1,21 +1,43 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, Literal
+from functools import partial
 
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from qutip import wigner
-import numpy as np
+
+from qutip import wigner, qfunc
 
 
-def plot_wigner(
-    state: np.ndarray, x: np.ndarray, y: np.ndarray, *, cmap: Union[None, str] = None, ax: Union[None, plt.Axes] = None
+def plot_representation(
+    rep_name: Literal["wigner", "qfunc"],
+    state: np.ndarray,
+    x: np.ndarray,
+    y: np.ndarray,
+    *,
+    cmap: Union[None, str] = None,
+    ax: Union[None, plt.Axes] = None
 ) -> Union[Tuple[plt.Axes, mpl.image.AxesImage], None]:
-    wigner_rep = wigner(state, x, y)
+    rep = None
+    if rep_name == "wigner":
+        rep = wigner(state, x, y)
+    elif rep_name == "qfunc":
+        rep = qfunc(state, x, y)
+
+    if rep is None:
+        raise Exception("Called with an invalid rep_name")
 
     fig = None
     if ax is None:
         fig, ax = plt.subplots()
 
-    img = ax.imshow(wigner_rep, extent=[min(x), max(x), min(y), max(y)], origin="lower", cmap=cmap)
+    img = ax.imshow(rep, extent=[min(x), max(x), min(y), max(y)], origin="lower", cmap=cmap)
     (fig or plt).colorbar(img, ax=ax)
 
+    ax.set_xlabel(r"$\rm{Re}(\alpha)$")
+    ax.set_ylabel(r"$\rm{Im}(\alpha)$")
+
     return ax, img
+
+
+plot_wigner = partial(plot_representation, "wigner")
+plot_qfunc = partial(plot_representation, "qfunc")
