ddab6f8621925e2367e616215d0c11a5c960ecbb
Oliver Dudgeon <22367286+OliverDudgeon@users.noreply.github.com>
2021-03-26 14:15:17 +0000

    Add reproduction of spin-1/2 sync plots

--- sync_calcs.py
+++ sync_calcs.py
@@ -1,6 +1,6 @@
+from functools import lru_cache
 import numpy as np
-from qutip import Qobj, expect, jmat, spin_state, steadystate
-from qutip.superoperator import lindblad_dissipator
+from qutip import Qobj, expect, jmat, spin_state, steadystate, lindblad_dissipator, spin_q_function
 from scipy import integrate
 from scipy.linalg import expm
 
@@ -46,13 +46,40 @@ def my_spin_q_func(density_op, theta, phi):
     return Q.reshape(Qsize)
 
 
-## Spin-1/2 Systems
+def spin_husimi_qfunc(density_op, theta, phi, *, method="su2"):
+    """Calculate the Spin Husimi Q function with several different methods"""
+    if method == "qutip":
+        Q, *_ = spin_q_function(density_op, theta, phi)
+        return Q
+    elif method == "vectorised":
+        return my_spin_q_func(density_op, theta, phi)
+    elif method == "su2":
+        st = np.sin(theta / 2)
+        ct = np.cos(theta / 2)
+        epip = np.exp(1j * phi / 2)
+        emip = np.exp(-1j * phi / 2)
+
+        A = st * emip
+        B = ct * epip
+
+        Q = (
+            A * A.conj() * density_op[1, 1]
+            + A.conj() * B * density_op[1, 0]
+            + A * B.conj() * density_op[0, 1]
+            + B * B.conj() * density_op[0, 0]
+        ) / (2 * np.pi)
+        assert np.count_nonzero(Q.imag) == 0
+        return Q.real
+
+
+"""Spin-1/2 Systems"""
 
 
 def signal_hamiltonian(detuning, signal_strength, J=0.5):
     return detuning * jmat(J, "z") + signal_strength * jmat(J, "y")
 
 
+@lru_cache(maxsize=None)
 def get_disipators(gain_amp, loss_amp, J=0.5):
     return (0.5 * gain_amp * lindblad_dissipator(jmat(J, "+")), 0.5 * loss_amp * lindblad_dissipator(jmat(J, "-")))
 
@@ -65,10 +92,7 @@ def get_disipators(gain_amp, loss_amp, J=0.5):
 def bloch_vector_comps(gain_amp, loss_amp, detuning, signal_strength):
     """From Parra-López & Bergli"""
     m_x = (
-        4
-        * signal_strength
-        * (gain_amp - loss_amp)
-        / ((gain_amp + loss_amp) ** 2 + 8 * (signal_strength ** 2 + 2 * detuning * 2))
+        4 * signal_strength * (gain_amp - loss_amp) / ((gain_amp + loss_amp) ** 2 + 8 * (signal_strength ** 2 + 2 * detuning * 2))
     )
 
     m_y = m_x * 4 * detuning / (gain_amp + loss_amp)
@@ -96,10 +120,7 @@ def calculate_steady_state(*, gain_amp, loss_amp, signal_strength, detuning):
     return steadystate(H, [gain, loss])
 
 
-def analytic_Q_function(mx, my, mz, phi, theta):
-    return (
-        1
-        + mx * np.cos(phi) * np.sin(theta)
-        + my * np.sin(phi) * np.sin(theta)
-        + mz * np.ones_like(phi) * np.cos(theta)
-    ) / (4 * np.pi)
+def bloch_Q_function(mx, my, mz, phi, theta):
+    return (1 + mx * np.cos(phi) * np.sin(theta) + my * np.sin(phi) * np.sin(theta) + mz * np.ones_like(phi) * np.cos(theta)) / (
+        4 * np.pi
+    )
